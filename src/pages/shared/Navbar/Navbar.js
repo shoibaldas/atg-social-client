e2ccd2daa9d52620473f4248f0c99ed02f088693
shoibaldas <shoibalshantu@gmail.com>
2022-12-07 23:24:36 +0600

    react router and routes has been intialize and basic setup is complete

---
+++ src/pages/shared/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                    Bootstrap
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
